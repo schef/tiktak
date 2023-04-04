f114aa35babc4a8ebe3ec7d38f82cc1ba7c6bbe7
schef <zvanstefan@gmail.com>
2023-04-04 16:14:18 +0200

    setted up the music and animation on the title screen

--- source/levels/title_screen.gd
+++ source/levels/title_screen.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
-@onready var comet = $AnimationPlayerComet
+@onready var animation = $AnimationPlayer
+@onready var button_sound = $AudioStreamPlayerButton
+@onready var button1 = $VBoxContainer/Button
+@onready var button2 = $VBoxContainer/Button2
+@onready var button3 = $VBoxContainer/Button3
+@onready var button4 = $VBoxContainer/Button4
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	comet.play("default")
-
+	animation.play("default")
+	button1.mouse_entered.connect(_on_mouse_entered)
+	button2.mouse_entered.connect(_on_mouse_entered)
+	button3.mouse_entered.connect(_on_mouse_entered)
+	button4.mouse_entered.connect(_on_mouse_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_mouse_entered():
+	button_sound.play(0.16)
