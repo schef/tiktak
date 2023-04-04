d457b0296fba5615b6f51e2b7ba047d04fefa536
Benjamin Horvat <budikutija@gmail.com>
2023-04-04 16:54:18 +0200

    new 5px font

--- source/levels/title_screen.gd
+++ source/levels/title_screen.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 @onready var animation = $AnimationPlayer
 @onready var button_sound = $AudioStreamPlayerButton
-@onready var button1 = $VBoxContainer/Button
-@onready var button2 = $VBoxContainer/Button2
-@onready var button3 = $VBoxContainer/Button3
-@onready var button4 = $VBoxContainer/Button4
+@onready var button1 = $CanvasLayer/VBoxContainer/Button
+@onready var button2 = $CanvasLayer/VBoxContainer/Button2
+@onready var button3 = $CanvasLayer/VBoxContainer/Button3
+@onready var button4 = $CanvasLayer/VBoxContainer/Button4
 
 
 func _ready():
